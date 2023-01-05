57df0ac9d369361dfddc15f2170ba94207697c1c
Gaurav <50616432+gaurav0991@users.noreply.github.com>
2023-01-05 13:24:20 +0530

    COM : dashboard formula changed

--- src/App.js
+++ src/App.js
@@ -12,10 +12,33 @@ function App() {
     let myNewForm = { ...form };
     myNewForm[event.target.name] = event.target.value;
     if (formula) {
-      myNewForm[out + index] = formula(myNewForm[args[0] + index], args[1]);
+      // myNewForm[out + index] = formula(myNewForm[args[0] + index], args[1]);
+      let formulaStr = formula;
+      let args = splitFormula(formula);
+      args.map((data) => {
+        if (typeof data == "string" && myNewForm[data + index]) {
+          formulaStr = formulaStr.replace(data, myNewForm[data + index]);
+        }
+      });
+      console.log(formulaStr);
+      myNewForm[out + index] = eval(formulaStr);
     }
     setFormData(myNewForm);
     console.log(myNewForm);
+  };
+
+  const splitFormula = function (arithmetic) {
+    var arr = [];
+    var operator = "/*+-^";
+
+    for (var i = 0; i < operator.length; i++) {
+      if (i == 0) {
+        arr = arithmetic.split(operator[i]);
+      } else {
+        arr = arr.toString().split(operator[i]);
+      }
+    }
+    return arr[0].split(",");
   };
   return (
     <div className="App">
